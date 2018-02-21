04ffc537477f12a80a6a906ad27b1573a2379786
amohan13 <anandmohanca31@gmail.com>
2018-02-21 09:29:40 +0530

    first maven project

---
+++ src/com/trainning/goal1/check.java
@@ -0,0 +1,13 @@
+package com.trainning.goal1;
+
+public class check {
+
+	public static void main(String[] args) {
+		int animal[]= {1,2,3};
+		for(int a:animal)
+		{
+			System.out.println(a);
+		}
+	}
+
+}
